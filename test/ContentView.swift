459bb5a3a66b18498f5b3a5843b7bb39b91b35df
jianhui <jia15176168273@icloud.com>
2023-12-13 12:18:13 +0800

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 贾建辉 on 2023/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 贾建辉 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
