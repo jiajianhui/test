bf6365d5c41601a529d2391381c25129818f800e
jianhui <jia15176168273@icloud.com>
2023-12-22 17:19:33 +0800

    3

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("002oo0000oooooooo")
+            Text("002oo000033oooooooo")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
